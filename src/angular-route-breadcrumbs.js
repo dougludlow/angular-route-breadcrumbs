85d355df9bde5c673fdcbea48b611542c7382efa
Douglas Ludlow <doug.ludlow@gmail.com>
2014-11-07 09:22:53 -0700

    Rename module.

--- src/angular-route-breadcrumbs.js
+++ src/angular-route-breadcrumbs.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('angular-route-breadcrumbs', [])
         .factory('breadcrumbs', breadcrumbs);
 
     breadcrumbs.$inject = ['$rootScope', '$route', '$location'];
